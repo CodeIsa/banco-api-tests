5fa6c0e95b79aebdf9069b13b1aa164a7921ec74
Isabelle Faria <48109634+CodeIsa@users.noreply.github.com>
2025-07-22 21:29:28 -0300

    adicionando testes de transferencia

---
+++ test/transferencia.test.js
@@ -0,0 +1,59 @@
+const request = require('supertest');
+const { expect } = require('chai');
+
+describe('Transferências', () => {
+  describe('POST /transferencias', () => {
+    it('Deve retornar sucesso com 201 quando o valor da transferencia for igual ou acima de R$ 10,00', async () => {
+      //Capturar o token
+      const respostaLogin = await request('http://localhost:3000')
+        .post('/login')
+        .set('Content-Type', 'application/json')
+        .send({
+          'username': 'julio.lima',
+          'senha': '123456'
+        })
+
+      const token = respostaLogin.body.token
+
+      const resposta = await request('http://localhost:3000')
+        .post('/transferencias')
+        .set('Authorization', `Bearer ${token}`)
+        .send({
+          //essas contas estao na tabela contas do banco de dados
+          contaOrigem: 1,
+          contaDestino: 2,
+          valor: 11,
+          token: ""
+        })
+
+      expect(resposta.status).to.equal(201)
+
+    })
+
+    it('Deve retornar falha com 422 com 201 quando o valor d atransferencia for abaixo de R$ 10,00', async () => {
+      const respostaLogin = await request('http://localhost:3000')
+        .post('/login')
+        .set('Content-Type', 'application/json')
+        .send({
+          'username': 'julio.lima',
+          'senha': '123456'
+        })
+
+      const token = respostaLogin.body.token
+
+      const resposta = await request('http://localhost:3000')
+        .post('/transferencias')
+        .set('Authorization', `Bearer ${token}`)
+        .send({
+          //essas contas estao na tabela contas do banco de dados
+          contaOrigem: 1,
+          contaDestino: 2,
+          valor: 7,
+          token: ""
+        })
+
+      expect(resposta.status).to.equal(422)
+    })
+  })
+
+})
