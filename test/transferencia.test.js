18c4acb8f4f8f7768f0f0f059739acac8a7c8d6e
CodeIsa <isaoliveira782@gmail.com>
2025-07-28 16:44:16 -0300

    Refactor authentication and transfer tests to use fixture data; add .gitignore and fixture files

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -1,52 +1,49 @@
-const request = require('supertest');
-const { expect } = require('chai');
-require('dotenv').config()
-const { obterToken } = require('../helpers/autenticacao')
+const request = require("supertest");
+const { expect } = require("chai");
+require("dotenv").config();
+const { obterToken } = require("../helpers/autenticacao");
+const postTransferencias = require("../fixtures/postTransferencias.json");
 
-describe('Transferências', () => {
-  describe('POST /transferencias', () => {
-    it('Deve retornar sucesso com 201 quando o valor da transferencia for igual ou acima de R$ 10,00', async () => {
-      const token = await obterToken('julio.lima', '123456')
+describe("Transferências", () => {
+  describe("POST /transferencias", () => {
+    let token;
+
+    beforeEach(async () => {
+      token = await obterToken("julio.lima", "123456");
+    });
+
+    it("Deve retornar sucesso com 201 quando o valor da transferencia for igual ou acima de R$ 10,00", async () => {
+      //usamos shalow copy quando temos esses 3 pontos, é uma copia superficial, usamos quando as propriedade não tem subniveis
+      const bodyTransferencias = { ...postTransferencias };
 
       const resposta = await request(process.env.BASE_URL)
-        .post('/transferencias')
-        .set('Authorization', `Bearer ${token}`)
-        .send({
-          //essas contas estao na tabela contas do banco de dados
-          contaOrigem: 1,
-          contaDestino: 2,
-          valor: 11,
-          token: ""
-        })
+        .post("/transferencias")
+        .set("Authorization", `Bearer ${token}`)
+        .send(bodyTransferencias);
 
-      expect(resposta.status).to.equal(201)
+      expect(resposta.status).to.equal(201);
+    });
 
-    })
+    it("Deve retornar falha com 422 com 201 quando o valor d atransferencia for abaixo de R$ 10,00", async () => {
+      const bodyTransferencias = { ...postTransferencias };
+      bodyTransferencias.valor = 7;
 
-    it('Deve retornar falha com 422 com 201 quando o valor d atransferencia for abaixo de R$ 10,00', async () => {
       const respostaLogin = await request(process.env.BASE_URL)
-        .post('/login')
-        .set('Content-Type', 'application/json')
+        .post("/login")
+        .set("Content-Type", "application/json")
         .send({
-          'username': 'julio.lima',
-          'senha': '123456'
-        })
+          username: "julio.lima",
+          senha: "123456",
+        });
 
-      const token = respostaLogin.body.token
+      const token = respostaLogin.body.token;
 
       const resposta = await request(process.env.BASE_URL)
-        .post('/transferencias')
-        .set('Authorization', `Bearer ${token}`)
-        .send({
-          //essas contas estao na tabela contas do banco de dados
-          contaOrigem: 1,
-          contaDestino: 2,
-          valor: 7,
-          token: ""
-        })
+        .post("/transferencias")
+        .set("Authorization", `Bearer ${token}`)
+        .send(bodyTransferencias);
 
-      expect(resposta.status).to.equal(422)
-    })
-  })
-
-})
+      expect(resposta.status).to.equal(422);
+    });
+  });
+});
